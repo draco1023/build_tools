f8f4be6f5f0377034fc36b39fc83f66486ab4ca8
Draco <draco1023@gmail.com>
2021-01-26 18:21:46 +0800

    git config mirrors

---
+++ git_proxy.sh
@@ -0,0 +1,5 @@
+git config --global url.https://gitclone.com/github.com/.insteadOf https://github.com/
+git config --global url.https://source.codeaurora.org/quic/lc/.insteadOf https://chromium.googlesource.com/
+git config --global url.https://source.codeaurora.org/quic/la/.insteadOf https://android.googlesource.com/
+git config --global url.https://gitee.com/mirrors/V8.git.insteadOf https://chromium.googlesource.com/v8/v8.git
+git config --global url.http://120.92.49.206:3232/chromiumsrc/boringssl.git.insteadOf https://boringssl.googlesource.com/boringssl.git
